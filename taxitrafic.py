9eb29f79a83f86c8ead384ff5d8aabb74b2fe462
seregaqt <91005021+seregaqt@users.noreply.github.com>
2023-10-27 16:03:00 +0300

    Add files via upload

--- taxitrafic.py
+++ taxitrafic.py
@@ -1,6 +1,6 @@
 class Taxi:
 
-    def __init__(self, traf,a,b):
+    def __init__(self, traf, a, b):
         self.traf = traf
         self.a = a
         self.b = b
@@ -11,10 +11,13 @@ class Taxi:
     def getFunctions(self):
         pass
 
+
 class Trafic(Taxi):
 
-    def __init__(self, traf):
-        self.traf = traf
+    def __init__(self, traf, a, b):
+        super().__init__(traf, a, b)
+        self.a = Taxi.a
+        self.b = Taxi.b
 
     def getTrafic(self):
         if self.traf == 'Э':
@@ -25,35 +28,36 @@ class Trafic(Taxi):
     def getFunctions(self):
         pass
 
+
 class Functions(Trafic):
 
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+    def __init__(self, a, b, traf):
+        super().__init__(traf, a, b)
 
     def getFunctions(self):
+
         if self.a == 1:
-            print('С ребёнком')
+            child = 'С ребёнком'
         else:
-            print('Без ребёнка')
+            child = 'Без ребёнка'
 
         if self.b == 1:
             print('С багажом')
         else:
             print('Без багажа')
+#
+#
+# def showTrafic(Taxi):
+#     print(Taxi.getTrafic())
+#
+#
+# def showFunctions(Trafic):
+#     print(Trafic.getFunctions())
 
-def showTrafic(Taxi):
-    print(Taxi.getTrafic())
 
-def showFunctions(Trafic):
-    print(Trafic.getFunctions())
-
-
-
-order_one = Taxi('Э', 0, 0)
+order_one = Taxi('Э', 0, 1)
 order_two = Taxi('К', 1, 1)
 
-showTrafic(order_one)
-showTrafic(order_two)
 
-
+# showTrafic(order_one)
+# showTrafic(order_two)
